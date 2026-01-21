9c405f7b7e19a429e8a71044db19282bd3631652
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2026-01-20 20:35:52 -0500

    Completed CH2 L4

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,21 @@
+import amqp from "amqplib";
+import node from "node:process";
+// import { process } from "node:process";
+
 async function main() {
   console.log("Starting Peril server...");
+
+  const connectionString = "amqp://guest:guest@localhost:5672/";
+  const amqpConnection = await amqp.connect(connectionString);
+
+  if (!amqpConnection) {
+    throw new Error("Error establishing connection to AMQP server")
+  }
+  console.log("Established connection with AMQP server")
+  
+  process.on('exit', () => {
+  console.log('Closing AMQP server connection');
+});
 }
 
 main().catch((err) => {
