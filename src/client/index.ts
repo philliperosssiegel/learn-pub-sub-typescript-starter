62ed861bc6a8233247d55b78e8e3c7f7d973222b
Phillipe Ross-Siegel <prosssiegel@gmail.com>
2026-01-26 18:14:59 -0500

    Completed CH3 L7

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,10 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { clientWelcome, commandStatus, getInput, printQuit, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -30,6 +33,39 @@ async function main() {
     PauseKey,
     SimpleQueueType.Transient,
   );
+  
+  const gameState = new GameState(username);
+
+  const loop = true;
+  while (loop) {
+    const input = await getInput();
+    if (input.length !== 0) {
+      const command = input[0];
+      switch (command) {
+        case "spawn":
+          commandSpawn(gameState, input)  
+        break;
+        case "move":
+          commandMove(gameState, input);
+          break;
+        case "status":
+          commandStatus(gameState);
+          break;
+        case "help":
+          printServerHelp();
+          break;
+        case "spam":
+          console.log("Spamming not allowed yet!");
+          break;
+        case "quit":
+          printQuit();
+          process.exit(0);
+        default:
+          console.log("Invalid input");
+          break;
+      }
+    }
+  }
 }
 
 main().catch((err) => {
